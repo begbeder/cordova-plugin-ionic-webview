585da6dd5e49f47bc2999736ddc2acd3568c2383
Begbeder <begbed.work@gmail.com>
2020-01-21 17:47:38 +0300

    [Utils] refactor: method

--- src/www/util.js
+++ src/www/util.js
@@ -24,6 +24,9 @@ var WebView = {
   },
   persistServerBasePath: function() {
     exec(null, null, 'IonicWebView', 'persistServerBasePath', []);
+  },
+  allowsBackForwardNavigationGestures: function (allow) {
+    exec(null, null, 'IonicWebView', 'allowsBackForwardNavigationGestures', [allow]);
   }
 }
 
